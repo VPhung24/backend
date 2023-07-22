45e6e64f5bb666dbefcc31f4b55067db1312b33d
Vivian Phung <dev+github@vivianphung.com>
2023-07-22 22:44:53 +0200

    Mint NFT on new user create

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-import csv
 from http.client import HTTPException
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from web3 import Web3
 
 from models import Restaurant, Review, User
 from firedantic import configure
 from google.cloud.firestore import Client
+import os
+from abi import tummy_abi
 
+load_dotenv()
 client = Client()
 
 configure(client, prefix="snacks-")
 
 app = FastAPI()
+w3 = Web3(Web3.HTTPProvider(os.environ["RPC_HTTP_URL"]))
+private_key = os.environ["DEPLOYER_PRIVATE_KEY"]
+tummy_contract_instance = w3.eth.contract(
+    address=os.environ["TUMMY_NFT_ADDRESS"], abi=tummy_abi
+)
 
 
 @app.get("/restaurants")
@@ -49,9 +58,21 @@ async def get_reviews(wallet_address: str) -> list[Review]:
 
 
 @app.post("/users/new")
-async def create_user(wallet_address: str) -> None:
+async def create_user(wallet_address: str) -> str:
     User(wallet_address=wallet_address).save()
-
+    # When a new user is created, we need to mint them a Tummy NFT
+    nonce = w3.eth.get_transaction_count('0xc4e1bf51752b4D55ef81FcA2334404245A07680c')
+    tx = tummy_contract_instance.functions.mintNFT(wallet_address, "test").buildTransaction(
+        {
+            "chainId": 5,
+            "gas": 1000000,
+            "gasPrice": w3.toWei("5", "gwei"),
+            "nonce": nonce,
+        }
+    )
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    return tx_hash
 
 @app.get("/users/{wallet_address}")
 async def get_user(wallet_address: str) -> User:
